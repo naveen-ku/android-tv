e1aa52396e80a6cc65a4570026a75988b1d4d2c0
naveen-ku <naveen2951996@gmail.com>
2024-07-07 22:21:48 +0530

    feat: add required dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -35,6 +37,48 @@ android {
 }
 
 dependencies {
-
+    val room_version = "2.6.1"
+    val lifecycle_version = "2.8.3"
+    val nav_version = "2.7.7"
     implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.leanback:leanback:1.0.0")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    // Room
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+    kapt("androidx.room:room-compiler:$room_version")
+    // Test helpers
+    testImplementation("androidx.room:room-testing:$room_version")
+    // Paging 3 Integration
+    implementation("androidx.room:room-paging:$room_version")
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    //okhttp
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor")
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
+    // View model
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    // For testing coroutines
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+    // For testing Android components with coroutines
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    // Recycler view
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    // Navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
+    // Feature module Support
+    implementation("androidx.navigation:navigation-dynamic-features-fragment:$nav_version")
+    // Testing Navigation
+    androidTestImplementation("androidx.navigation:navigation-testing:$nav_version")
+    // Testing - junit5 & expresso
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    // Firebase
+    implementation(platform("com.google.firebase:firebase-bom:33.1.1"))
+    implementation("com.google.firebase:firebase-auth")
 }
