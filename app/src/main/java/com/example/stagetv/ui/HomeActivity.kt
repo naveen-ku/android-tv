ef5ba1753dec19a72f6c7896cf86a2dc93a8ba18
naveen-ku <naveen2951996@gmail.com>
2024-07-11 22:42:45 +0530

    feat: home page basiclayout

--- app/src/main/java/com/example/stagetv/ui/HomeActivity.kt
+++ app/src/main/java/com/example/stagetv/ui/HomeActivity.kt
@@ -3,10 +3,8 @@ package com.example.stagetv.ui
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.fragment.app.FragmentActivity
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
+import com.example.stagetv.R
 import com.example.stagetv.databinding.ActivityHomeBinding
-import com.example.stagetv.viewmodel.AuthViewModel
 import com.example.stagetv.viewmodel.HomeViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,17 +13,18 @@ class HomeActivity : FragmentActivity() {
     private lateinit var binding: ActivityHomeBinding
     private val homeViewModel: HomeViewModel by viewModels<HomeViewModel>()
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-//        homeViewModel = ViewModelProvider(this)[HomeViewModel::class.java]
+//        homeViewModel =   ViewModelProvider(this)[HomeViewModel::class.java]
 
-        homeViewModel.getPopularMovies()
-        homeViewModel.getMoviesList()
+//        homeViewModel.getPopularMovies()
+//        homeViewModel.getMoviesList()
+        val listFragment = ListFragment()
+        supportFragmentManager.beginTransaction()
+            .add(R.id.list_fragment, listFragment).commit()
 
     }
 }
